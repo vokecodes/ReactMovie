a554f7a080921e81cf7dbf1c68b1a1eff97d360a
voke junior <vefe44@gmail.com>
2019-07-09 01:08:44 +0100

    set up the api to fetch data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from "../../config";
 import HeroImage from "../elements/HeroImage/HeroImage";
 import SearchBar from "../elements/SearchBar/SearchBar";
 import FourColGrid from "../elements/FourColGrid/FourColGrid";
@@ -8,7 +15,29 @@ import Spinner from "../elements//Spinner/Spinner";
 import "./Home.css";
 
 class Home extends Component {
-  state = {};
+  state = {
+    movies: [],
+    heroImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ""
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  }
+
+  fetchItems = endpoint => {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
   render() {
     return (
       <div className="rmdb-home">
