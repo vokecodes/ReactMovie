71e4a2721a043125411e01480465d8578f109655
voke junior <vefe44@gmail.com>
2019-07-13 17:56:47 +0100

    done with the home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,22 +30,98 @@ class Home extends Component {
     this.fetchItems(endpoint);
   }
 
+  searchItems = searchTerm => {
+    let endpoint = "";
+    this.setState({
+      movies: [],
+      loading: true,
+      searchTerm: ""
+    });
+    console.log(searchTerm);
+
+    if (searchTerm === "") {
+      endpoint = `${API_URL}movie/popular?api=${API_KEY}&language=en-US&page=${this
+        .state.currentPage + 1}`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+    }
+
+    this.fetchItems(endpoint);
+  };
+
   fetchItems = endpoint => {
     fetch(endpoint)
       .then(result => result.json())
       .then(result => {
-        console.log(result);
+        this.setState({
+          movies: [this.state.movies, ...result.results],
+          heroImage: this.state.heroImage || result.results[0],
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.total_pages
+        });
       });
+  };
+
+  loadMoreItems = () => {
+    let endpoint = "";
+    this.setState({ loading: true });
+
+    if (this.state.searchTerm === "") {
+      endpoint = `${API_URL}movie/popular?api=${API_KEY}&language=en-US&page=${this
+        .state.currentPage + 1}`;
+    } else {
+      endpoint = `${API_URL}search/movie?api=${API_KEY}&language=en-US&query=${
+        this.state.searchTerm
+      }&page=${this.state.currentPage + 1}`;
+    }
+
+    this.fetchItems(endpoint);
   };
 
   render() {
     return (
       <div className="rmdb-home">
-        <HeroImage />
-        <SearchBar />
-        <FourColGrid />
-        <Spinner />
-        <LoadMoreBtn />
+        {this.state.heroImage ? (
+          <div>
+            <HeroImage
+              image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${
+                this.state.heroImage.backdrop_path
+              }`}
+              title={this.state.heroImage.original_title}
+              text={this.state.heroImage.overview}
+            />
+            <SearchBar callback={this.searchItems} />
+          </div>
+        ) : null}
+
+        <div className="rmdb-home-grid">
+          <FourColGrid
+            header={this.state.searchTerm ? "Search Result" : "Popular Movie"}
+            loading={this.state.loading}
+          >
+            {this.state.movies.map((element, i) => {
+              return (
+                <MovieThumb
+                  key={i}
+                  clickable={true}
+                  image={
+                    element.poster_path
+                      ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}`
+                      : "./images/no_image.jpg"
+                  }
+                  movieId={element.id}
+                  movieName={element.original_title}
+                />
+              );
+            })}
+          </FourColGrid>
+          {this.state.loading ? <Spinner /> : null}
+          {this.state.currentPage <= this.state.totalPages &&
+          !this.state.loading ? (
+            <LoadMoreBtn text="Load More" onClick={this.loadMoreItems} />
+          ) : null}
+        </div>
       </div>
     );
   }
